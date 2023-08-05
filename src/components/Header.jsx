1f00dc511972e624a49c738c0575f419ea081aa0
Dineshwar <dineshwar.rajendran@gmail.com>
2023-08-05 21:43:18 +0530

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">PomoZen</a>
+      </div>
+      <div className="flex-none">
+        <button
+          className="btn btn-square btn-ghost"
+          onClick={() => window.my_modal_5.showModal()}
+        >
+          <svg
+            className="h-6 w-6 text-white"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {" "}
+            <path stroke="none" d="M0 0h24v24H0z" />{" "}
+            <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />{" "}
+            <circle cx="12" cy="12" r="3" />
+          </svg>
+        </button>
+      </div>
+      {/* Open the modal using ID.showModal() method */}
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <form method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn">Close</button>
+          </div>
+        </form>
+      </dialog>
+    </div>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "cyberpunk",
+      {
+        pomodoro: {
+          "primary": "#78eddb",
+          "secondary": "#0c3d7c",
+          "accent": "#d6005c",
+          "neutral": "#362938",
+          "base-100": "#33293d",
+          "info": "#48bbf4",
+          "success": "#24a899",
+          "warning": "#f1b037",
+          "error": "#f56751",
+        },
+      },
+    ],
+  },
+};
